6dfa403827ac855fccc60d695ca1a3c7c0be91bd
Farid Vatani <faridvatani@gmail.com>
2024-08-19 11:26:19 +0330

    feat: Implement footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { CallToAction } from "@/sections/CallToAction";
 import { Features } from "@/sections/Features";
+import { Footer } from "@/sections/Footer";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
@@ -14,6 +15,7 @@ export default function Home() {
       <Features />
       <Testimonials />
       <CallToAction />
+      <Footer />
     </main>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,41 @@
+import { Logo, SocialX, SocialInstagram, SocialYoutube } from "@/assets";
+import Link from "next/link";
+
+const navItems = [
+  { href: "#", title: "Features" },
+  { href: "#", title: "Developers" },
+  { href: "#", title: "Company" },
+  { href: "#", title: "Blog" },
+  { href: "#", title: "Changelog" },
+];
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="py-5 border-t border-white/15">
+      <div className="container">
+        <div className="flex flex-col lg:flex-row lg:items-center gap-8">
+          <div className="flex gap-2 items-center lg:flex-1">
+            <Logo className="size-6" />
+            <span className="font-medium">AI Startup Landing Page</span>
+          </div>
+          <nav className="flex flex-col lg:flex-row gap-5 lg:gap-7 lg:flex-1 lg:justify-center">
+            {navItems.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                className="text-white/70 hover:text-white text-xs md:text-sm transition"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+          <div className="flex gap-5 lg:flex-1 lg:justify-end">
+            <SocialX className="text-white/40 hover:text-white transition" />
+            <SocialInstagram className="text-white/40 hover:text-white transition" />
+            <SocialYoutube className="text-white/40 hover:text-white transition" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
